599f87e83d291ddaadb67ef326f6d877cdd00a61
fifnst <afif.nasution@gmail.com>
2017-12-25 20:51:58 +0700

    test

--- app/src/main/java/hasibuan/sabtusore/Alarm.java
+++ app/src/main/java/hasibuan/sabtusore/Alarm.java
@@ -28,7 +28,7 @@ public class Alarm extends Activity {
     //private NotificationReceiver mReceiver = new NotificationReceiver();
     public int bisa = 1;
 
-    //tes
+    //tes lagi
 
     TimePicker picker;
     ImageButton buttonStart;
