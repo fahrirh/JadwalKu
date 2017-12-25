fc325e9d44bfb1bade85c6e51cdbbcdb62ef3af6
fahrirh <hasibuanfahri@gmail.com>
2017-12-26 01:03:55 +0800

    welcome afif

--- app/src/main/java/hasibuan/sabtusore/Alarm.java
+++ app/src/main/java/hasibuan/sabtusore/Alarm.java
@@ -28,7 +28,7 @@ public class Alarm extends Activity {
     //private NotificationReceiver mReceiver = new NotificationReceiver();
     public int bisa = 1;
 
-    //bismillah timepicker sudah oke
+    //bismillah timepicker sudah oke oke
 
     TimePicker picker;
     ImageButton buttonStart;
