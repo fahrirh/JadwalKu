03e6dbfaf88ecfb82777d4b7d5eab0b91a5224f0
fahrirh <hasibuanfahri@gmail.com>
2017-12-25 23:33:20 +0800

    change to info

--- app/src/main/java/hasibuan/sabtusore/AlarmReceiver.java
+++ app/src/main/java/hasibuan/sabtusore/AlarmReceiver.java
@@ -199,7 +199,7 @@ public class AlarmReceiver extends WakefulBroadcastReceiver {
         AlarmManagerUtil alarmUtil = new AlarmManagerUtil();
         alarmUtil.initAlarmNotification(context);
 
-        //Find dayy
+        //Find dayyy
         Calendar calendar = Calendar.getInstance();
         int currentDay = calendar.get(Calendar.DAY_OF_WEEK);
 
