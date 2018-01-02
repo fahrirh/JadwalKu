cc5d6cab10fedf0aa8e50ceed142f6900ab12f64
fahrirh <hasibuanfahri@gmail.com>
2018-01-02 12:53:23 +0800

    fix bug alarm

--- app/src/main/java/hasibuan/sabtusore/Const.java
+++ app/src/main/java/hasibuan/sabtusore/Const.java
@@ -13,7 +13,7 @@ import java.util.Calendar;
 
 public class Const extends Activity{
     public static final int[] ALARM_HOUR_TIME = {6, 8, 12,14,16};
-    public static final int[] ALARM_MINUTE_TIME = {30, 00, 00, 00, 00};
+    public static final int[] ALARM_MINUTE_TIME = {30, 00, 00, 00, 02};
 
 }
 
